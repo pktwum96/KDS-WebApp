f84c441400384d93798ee5e3b9e54fb4a8bba235
Kofi Twum-Ampofo <pktwum96@gmail.com>
2019-06-22 15:38:00 +0000

    Fixed issues

--- src/Contact/Form.js
+++ src/Contact/Form.js
@@ -9,21 +9,21 @@ class Form extends React.Component {
          <form className="pb-5" data-netlify="true">
            <div className="form-group">
              <label htmlFor="contactName">Name</label>
-             <input type="text" className="form-control" id="contactName" placeholder="Akwasi Mintah"/>
+             <input type="text" name="name" className="form-control" id="contactName" placeholder="Akwasi Mintah"/>
            </div>
              <div className="form-group">
                <label htmlFor="contactEmail">Email address</label>
-               <input type="email" className="form-control" id="contactEmail" placeholder="name@example.com"/>
+               <input type="email" name="email" className="form-control" id="contactEmail" placeholder="name@example.com"/>
              </div>
                <div className="form-group">
                  <label htmlFor="contactNumber">Phone Number</label>
-                 <input type="number" className="form-control" id="contactNumber" placeholder="024194119"/>
+                 <input type="number" name="number" className="form-control" id="contactNumber" placeholder="024194119"/>
                </div>
            <div className="form-group">
              <label htmlFor="contactText">Message</label>
-             <textarea className="form-control" id="contactText" rows="3"></textarea>
+             <textarea  name="message" className="form-control" id="contactText" rows="3"></textarea>
            </div>
-           <button className="btn btn-outline-success">Submit</button>
+           <button className="btn btn-outline-success" type="submit">Submit</button>
          </form>
     )
   }
